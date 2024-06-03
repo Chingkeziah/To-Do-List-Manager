402061aa436db5a85a6c4a709942ef94820a9ad2
Chingkeziah <chingkeziah@gmail.com>
2024-06-03 23:08:57 +0100

    feat: stored tasks in an array of objects

--- src/to-do.js
+++ src/to-do.js
@@ -1,4 +1,32 @@
-document.getElementById('addButton')
-function add(){
-    
-}
+let task = [
+    {
+        taskName: 'Work',
+        description: "Complete project report",
+        dueDate: Date('10-06-24'),
+        priority: 'important',
+    },
+    {
+        taskName: 'School',
+        description: "Complete final year thesis",
+        dueDate: Date('15-07-24'),
+        priority: 'very important',
+    },
+    {
+        taskName: 'Errands',
+        description: "Buy groceries from the store",
+        dueDate: Date('05-06-24'),
+        priority: 'important',
+    },
+    {
+        taskName: 'Health and fitness',
+        description: "Go to the hospital for checkup",
+        dueDate: Date('08-06-24'),
+        priority: 'very important',
+    },
+    {
+        taskName: 'Personal Development',
+        description: "Read 10 pages of a self-development book",
+        dueDate: Date('04-06-24'),
+        priority: 'important',
+    }
+]
