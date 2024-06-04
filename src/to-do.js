8c9c4e64c825c1aef738d397a02d4260b0a8184a
Chingkeziah <chingkeziah@gmail.com>
2024-06-04 22:17:17 +0100

    feat:

--- src/to-do.js
+++ src/to-do.js
@@ -1,32 +1,42 @@
 let task = [
     {
+        id: 1,
         taskName: 'Work',
         description: "Complete project report",
         dueDate: Date('10-06-24'),
         priority: 'important',
     },
     {
+        id:2,
         taskName: 'School',
         description: "Complete final year thesis",
         dueDate: Date('15-07-24'),
         priority: 'very important',
     },
     {
+        id:3,
         taskName: 'Errands',
         description: "Buy groceries from the store",
         dueDate: Date('05-06-24'),
         priority: 'important',
     },
     {
+        id:4,
         taskName: 'Health and fitness',
         description: "Go to the hospital for checkup",
         dueDate: Date('08-06-24'),
         priority: 'very important',
     },
     {
+        id:5,
         taskName: 'Personal Development',
         description: "Read 10 pages of a self-development book",
         dueDate: Date('04-06-24'),
         priority: 'important',
     }
 ]
+
+function add(){
+    addEventListener
+}
+
