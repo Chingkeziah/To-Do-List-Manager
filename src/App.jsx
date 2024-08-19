d7a8a47bdd44c9e2ede0b989e20b512d49b2d295
Chingkeziah <chingkeziah@gmail.com>
2024-08-19 23:36:54 +0100

    feat: installed browser router for routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import Home from './Components/Pages/Home';
 import Header from './Components/Layout/Header';
 import Footer from './Components/Layout/Footer';
 import Todo from './Components/Pages/To-do';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      {/* <Home /> */}
-      <To-do />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/to-do" element={<to-do />} />
+        </Routes>
+      </Router>
     </>
   )
 }
